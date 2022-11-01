049a6e3338fa6a7092dd3faad0491c403d94a1d3
Ryan Cortino <ryancortino@outlook.com>
2022-11-01 07:14:48 -0500

    Adds files for Price value object. Clean up unused files.

---
+++ src/ReceiptGenerator.Domain/ValueObjects/Price.cs
@@ -0,0 +1,37 @@
+﻿using ReceiptGenerator.Domain.Common;
+
+namespace ReceiptGenerator.Domain.ValueObjects;
+
+/// <summary>
+/// This value object represents 
+/// </summary>
+public class Price : ValueObject
+{
+    #region Constructors
+    static Price()
+    {
+    }
+
+    private Price()
+    {
+    }
+
+    private Price(decimal value)
+    {
+        Value = value;
+    }
+    #endregion
+
+    #region Static Members
+
+    #endregion
+
+    #region Fields & Properties
+    public decimal Value { get; private set; } = 0m;
+    #endregion
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
