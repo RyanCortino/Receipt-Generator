a41fd02e496ac2eaf54373558c16864e8686f781
Ryan Cortino <ryancortino@outlook.com>
2022-11-01 07:04:25 -0500

    Adds exception handling for Unsupported Values.

--- src/ReceiptGenerator.Domain/Exceptions/UnsupportedValueException.cs
+++ src/ReceiptGenerator.Domain/Exceptions/UnsupportedValueException.cs
@@ -5,11 +5,23 @@ namespace ReceiptGenerator.Domain.Exceptions;
 public class UnsupportedCouponCodeException : Exception
 {
     public UnsupportedCouponCodeException(string couponCode)
-        : base($"Coupon code \"{couponCode}\" is unsupported.") { }
+        : base($"Coupon code \"{couponCode}\" is unsupported.")
+    {
+    }
 }
 
 public class UnsupportedItemException : Exception
 {
     public UnsupportedItemException(Item item)
-        : base($"Item \"{item}\" is unsupported.") { }
+        : base($"Item \"{item}\" is unsupported.")
+    {
+    }
+}
+
+public class UnsupportedTaxClassification : Exception
+{
+    public UnsupportedTaxClassification(TaxClassification taxClassification)
+        : base($"TaxClassification \"{taxClassification}\" is unsupported")
+    {
+    }
 }
