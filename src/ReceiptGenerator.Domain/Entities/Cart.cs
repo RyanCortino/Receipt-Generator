c9bbd2665c8c4d61e134052ba2a524678a0953ae
Ryan Cortino <ryancortino@outlook.com>
2022-11-01 07:15:45 -0500

    Adds files for Cart entity class.

---
+++ src/ReceiptGenerator.Domain/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿using ReceiptGenerator.Domain.Common;
+
+namespace ReceiptGenerator.Domain.Entities;
+
+public class Cart : IHasDomainEvent
+{
+    public IList<DomainEvent> DomainEvents { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
