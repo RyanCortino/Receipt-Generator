4c2a2c5f90aa276e23bf46e3a9925ffb0199427d
Ryan Cortino <ryancortino@outlook.com>
2022-11-01 07:15:23 -0500

    Adds entities for Products and LineItems

---
+++ src/ReceiptGenerator.Domain/Entities/LineItem.cs
@@ -0,0 +1,40 @@
+﻿using ReceiptGenerator.Domain.Common;
+
+namespace ReceiptGenerator.Domain.Entities;
+
+public class LineItem : IHasDomainEvent
+{
+    public Product? Product { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal? LinePrice
+    {
+        get
+        {
+            if (Product == null) 
+            {
+                return Product?.Price * Quantity;
+            }
+
+            return Product?.Price * Quantity;
+        }
+    }
+
+    private decimal? _discount;
+    public decimal Discount
+    {
+        get => _discount ?? 0;
+        set
+        {
+            if (value == _discount || _discount is null)
+            {
+                DomainEvents.Add(new LineItemDiscountedEvent(this));
+            }
+
+            _discount = value;
+        }
+    }
+
+    public IList<DomainEvent> DomainEvents { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
---
+++ src/ReceiptGenerator.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using ReceiptGenerator.Domain.Common;
+using ReceiptGenerator.Domain.ValueObjects;
+
+namespace ReceiptGenerator.Domain.Entities;
+
+public class Product
+{
+    public string Name { get; set; }
+
+    public TaxClassification TaxType { get; set; }
+
+    public decimal Price { get; set; }
+
+    public bool IsTaxExempt => (TaxType == TaxClassification.Medical || TaxType == TaxClassification.Food);
+}
