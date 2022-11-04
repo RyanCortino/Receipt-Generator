0702974b4007264ccabb6c4707588df147c8bacc
Ryan Cortino <ryancortino@outlook.com>
2022-11-04 10:16:18 -0500

    Adds CreateProduct command and handler.

---
+++ src/Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using ReceiptGenerator.Application.Common.Interfaces;
+using ReceiptGenerator.Domain.Entities;
+using ReceiptGenerator.Domain.Events;
+using ReceiptGenerator.Domain.ValueObjects;
+
+namespace ReceiptGenerator.Application.Products.Commands.CreateProduct;
+
+public class CreateProductCommand : IRequest<int>
+{
+    public int CartId { get; init; }
+    
+    public string? ProductName { get; init; }
+
+    public TaxClassification? TaxType { get; init; }
+    
+    public Money? Price{ get; init; }
+}
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateProductCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new Product()
+        {
+            CartId = request.CartId,
+            Name = request.ProductName,
+            TaxType = request.TaxType,
+            Price = request.Price
+        };
+
+        entity.AddDomainEvent(new ProductCreatedEvent(entity));
+
+        _context.Products.Add(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity.Id;
+    }
+}
---
+++ src/Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System.Collections.Specialized;
+
+namespace ReceiptGenerator.Application.Products.Commands.CreateProduct;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(v => v.ProductName)
+            .NotEmpty().WithMessage("Product Name is required.");
+
+        RuleFor(v => v.TaxType)
+            .NotNull().WithMessage("TaxType is required.");
+
+        RuleFor(v => v.Price)
+            .NotNull().WithMessage("Price is required.");
+    }
+}
