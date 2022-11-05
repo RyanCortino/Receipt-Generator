222f8549d0b17daf94b6705f7d988caeac0d1266
Ryan Cortino <ryancortino@outlook.com>
2022-11-05 11:09:53 -0500

    Adds ProductDto to application layer.

---
+++ src/ReceiptGenerator.Application/Products/Queries/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using ReceiptGenerator.Domain.Entities;
+
+namespace ReceiptGenerator.Application.Products.Queries;
+
+public class ProductDto // : IMapFrom<Product>
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? TaxCategory { get; set; }
+}
