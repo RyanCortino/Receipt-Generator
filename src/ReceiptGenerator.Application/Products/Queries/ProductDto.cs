3635ecb8e08e3598c6939dbb2b18732d6b2388e3
Ryan Cortino <ryancortino@outlook.com>
2022-11-08 08:51:48 -0600

    Adds a compelte dto for the Product aggregate.

--- src/ReceiptGenerator.Application/Products/Queries/ProductDto.cs
+++ src/ReceiptGenerator.Application/Products/Queries/ProductDto.cs
@@ -2,7 +2,7 @@
 
 namespace ReceiptGenerator.Application.Products.Queries;
 
-public class ProductDto // : IMapFrom<Product>
+public class ProductModel // : IMapFrom<Product>
 {
     public int Id { get; set; }
 
