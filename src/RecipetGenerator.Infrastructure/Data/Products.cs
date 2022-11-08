7178c82b52d1f0bba882e71acfb4ca880e495948
Ryan Cortino <ryancortino@outlook.com>
2022-11-08 08:52:18 -0600

    Adds implementation for Products repository.

--- src/RecipetGenerator.Infrastructure/Data/Products.cs
+++ src/RecipetGenerator.Infrastructure/Data/Products.cs
@@ -3,7 +3,7 @@ using ReceiptGenerator.Domain.Common.Interfaces;
 
 namespace RecipetGenerator.Infrastructure.Data;
 
-public class Products : IProductRepo
+public class Products : IRepository<ProductModel>
 {
     private readonly ISqlDataAccess _db;
 
@@ -12,24 +12,24 @@ public class Products : IProductRepo
         _db = db;
     }
 
-    public async Task<ProductDto?> GetProduct(int id)
+    public async Task<ProductModel?> Get(int id)
     {
-        var results = await _db.LoadData<ProductDto, dynamic>(
+        var results = await _db.LoadData<ProductModel, dynamic>(
             storedProcedure: "dbo.spProduct_Get",
             new { Id = id });
 
         return results.FirstOrDefault();
     }
 
-    public Task<IEnumerable<ProductDto>> GetProducts() =>
-        _db.LoadData<ProductDto, dynamic>(storedProcedure: "dbo.spProduct_GetAll", new { });
+    public Task<IEnumerable<ProductModel>> GetAll() =>
+        _db.LoadData<ProductModel, dynamic>(storedProcedure: "dbo.spProduct_GetAll", new { });
 
-    public Task InsertProduct(ProductDto product) =>
+    public Task Insert(ProductModel product) =>
         _db.SaveData(storedProcedure: "dbo.spProduct_Insert", new { product.Name, product.Price, product.TaxCategory });
 
-    public Task UpdateProduct(ProductDto product) =>
+    public Task Update(ProductModel product) =>
         _db.SaveData(storedProcedure: "dbo.spUser_Update", product);
 
-    public Task DeleteProduct(int id) =>
+    public Task Delete(int id) =>
         _db.SaveData(storedProcedure: "dbo.spUser_Delete", new { Id = id });
 }
