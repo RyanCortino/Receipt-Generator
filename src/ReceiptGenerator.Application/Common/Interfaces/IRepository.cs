073681d66b19ffe9658dae04441821b8b0b2a252
Ryan Cortino <ryancortino@outlook.com>
2022-11-08 08:50:59 -0600

    Adds interface for data repositories.

---
+++ src/ReceiptGenerator.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReceiptGenerator.Application.Products.Queries;
+
+namespace RecipetGenerator.Infrastructure.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        Task Delete(int id);
+        Task<T?> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task Insert(T product);
+        Task Update(T product);
+    }
+}
