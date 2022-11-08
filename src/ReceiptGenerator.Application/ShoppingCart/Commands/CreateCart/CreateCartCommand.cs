be6ed1026914f92122b854c18d958302e262a964
Ryan Cortino <ryancortino@outlook.com>
2022-11-08 10:07:36 -0600

    Adds Command for creating cart.

---
+++ src/ReceiptGenerator.Application/ShoppingCart/Commands/CreateCart/CreateCartCommand.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using ReceiptGenerator.Application.Common.Interfaces;
+using ReceiptGenerator.Domain.Entities;
+
+namespace ReceiptGenerator.Application.ShoppingCart.Commands.CreateCart;
+
+public record CreateCartCommand : IRequest<int>
+{
+    public string? Title { get; init; }
+}
+
+public class CreateCartCommandHandler : IRequestHandler<CreateCartCommand, int>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public CreateCartCommandHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> Handle(CreateCartCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new Cart
+        {
+            Title = request.Title
+        };
+
+        await _dbContext.Carts.InsertAsync(entity);
+
+        return entity.Id;
+    }
+}
---
+++ src/ReceiptGenerator.Application/ShoppingCart/Commands/CreateCart/CreateCartCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using ReceiptGenerator.Application.Common.Interfaces;
+
+namespace ReceiptGenerator.Application.ShoppingCart.Commands.CreateCart;
+
+public class CreateCartCommandValidator : AbstractValidator<CreateCartCommand>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public CreateCartCommandValidator(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+
+        RuleFor(v => v.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(50).WithMessage("Title must not exceed 50 characters.")
+            .MustAsync(BeUniqueTitle).WithMessage("The requested title already exists.");
+    }
+
+    public async Task<bool> BeUniqueTitle(string title, CancellationToken cancellationToken)
+    {
+        var carts = await _dbContext.Carts.GetAllAsync();
+        return carts.First(s => s.Title == title) != null;
+    }
+}
