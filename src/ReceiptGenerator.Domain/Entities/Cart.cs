5f77114fcd222f6a10acff064a5918ccd25268f9
Ryan Cortino <ryancortino@outlook.com>
2022-11-08 10:08:49 -0600

    Adds fields to Cart Entity

--- src/ReceiptGenerator.Domain/Entities/Cart.cs
+++ src/ReceiptGenerator.Domain/Entities/Cart.cs
@@ -1,5 +1,9 @@
-﻿namespace ReceiptGenerator.Domain.Entities;
+﻿using Microsoft.VisualBasic;
+
+namespace ReceiptGenerator.Domain.Entities;
 
 public class Cart : BaseEntity
 {
+    public string? Title { get; set; }
+    public IEnumerable<LineItem> Items { get; private set; } = new List<LineItem>();
 }
